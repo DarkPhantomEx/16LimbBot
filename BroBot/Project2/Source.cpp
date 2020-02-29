a8938591c7d04aee7b1a4583207376f18af3694f
DarkPhantomEx <54686142+DarkPhantomEx@users.noreply.github.com>
2020-02-29 17:43:24 -0500

    Brobot has been drawn.

--- BroBot/Project2/Source.cpp
+++ BroBot/Project2/Source.cpp
@@ -39,7 +39,7 @@ struct vert {
 class Rect {
 public:
 	vert V[4];
-	float colors[3 * NUM_OF_PARTS];
+	float colors[3];
 }R[NUM_OF_PARTS];
 
 //Parameters for the Window & Canvas
@@ -59,8 +59,62 @@ int current;
 void init(){
 
 	/// Initialize Rect vertices
+	//LB
+	R[0].V[0].x = -2; R[0].V[0].y = 0;	R[0].V[1].x = -2; R[0].V[1].y = 1;
+	R[0].V[2].x =  2; R[0].V[2].y = 1;	R[0].V[3].x =  2; R[0].V[3].y = 0;
+	//RT
+	R[1].V[0].x = -0.5f; R[1].V[0].y =  0;	R[1].V[1].x = -0.5f; R[1].V[1].y = -2;
+	R[1].V[2].x =  0.5f; R[1].V[2].y = -2;	R[1].V[3].x =  0.5f; R[1].V[3].y =  0;
+	
+	R[2].V[0].x = -0.5f; R[2].V[0].y =  0;	R[2].V[1].x = -0.5f; R[2].V[1].y = -2;
+	R[2].V[2].x =  0.5f; R[2].V[2].y = -2;	R[2].V[3].x =  0.5f; R[2].V[3].y =  0;
+	
+	R[3].V[0].x = -1.5f; R[3].V[0].y = 0;	R[3].V[1].x = -1.5f; R[3].V[1].y = -1;
+	R[3].V[2].x =  0.5f; R[3].V[2].y = -1;	R[3].V[3].x =  0.5f; R[3].V[3].y = 0;
+	//RTend
+	//LT
+	R[4].V[0].x = -0.5f; R[4].V[0].y =  0;	R[4].V[1].x = -0.5f; R[4].V[1].y = -2;
+	R[4].V[2].x =  0.5f; R[4].V[2].y = -2;	R[4].V[3].x =  0.5f; R[4].V[3].y =  0;
+	
+	R[5].V[0].x = -0.5f; R[5].V[0].y =  0;	R[5].V[1].x = -0.5f; R[5].V[1].y = -2;
+	R[5].V[2].x =  0.5f; R[5].V[2].y = -2;	R[5].V[3].x =  0.5f; R[5].V[3].y =  0;
+	
+	R[6].V[0].x = -0.5f; R[6].V[0].y = 0;	R[6].V[1].x = -0.5f; R[6].V[1].y = -1;
+	R[6].V[2].x =  1.5f; R[6].V[2].y = -1;	R[6].V[3].x =  1.5f; R[6].V[3].y = 0;
+	//LTend
+	//UB
+	R[7].V[0].x = -3; R[7].V[0].y = 0;	R[7].V[1].x = -3; R[7].V[1].y = 2;
+	R[7].V[2].x =  3; R[7].V[2].y = 2;	R[7].V[3].x =  3; R[7].V[3].y = 0;
+	//RA
+	R[8].V[0].x =    0; R[8].V[0].y =  0.5f;	R[8].V[1].x = -1.5f; R[8].V[1].y =  0.5f;
+	R[8].V[2].x = -1.5f; R[8].V[2].y = -0.5f;	R[8].V[3].x =    0; R[8].V[3].y = -0.5f;
 
-	int current = -1; 
+	R[9].V[0].x =    0; R[9].V[0].y =  0.5f;	R[9].V[1].x = -1.5f; R[9].V[1].y =  0.5f;
+	R[9].V[2].x = -1.5f; R[9].V[2].y = -0.5f;	R[9].V[3].x =    0; R[9].V[3].y = -0.5f;
+	
+	R[10].V[0].x = -0.5f; R[10].V[0].y =  0.75f;	R[10].V[1].x =     0; R[10].V[1].y =  0.75f;
+	R[10].V[2].x =     0; R[10].V[2].y = -0.75f;	R[10].V[3].x = -0.5f; R[10].V[3].y = -0.75f;
+	//RAend
+	//LA
+	R[11].V[0].x =    0; R[11].V[0].y =  0.5f;	R[11].V[1].x = 1.5f; R[11].V[1].y =  0.5f;
+	R[11].V[2].x = 1.5f; R[11].V[2].y = -0.5f;	R[11].V[3].x =    0; R[11].V[3].y = -0.5f;
+
+	R[12].V[0].x =    0; R[12].V[0].y =  0.5f;	R[12].V[1].x = 1.5f; R[12].V[1].y =  0.5f;
+	R[12].V[2].x = 1.5f; R[12].V[2].y = -0.5f;	R[12].V[3].x =    0; R[12].V[3].y = -0.5f;
+	
+	R[13].V[0].x = 0.5f; R[13].V[0].y =  0.75f;	R[13].V[1].x =     0; R[13].V[1].y =  0.75f;
+	R[13].V[2].x =     0; R[13].V[2].y = -0.75f;	R[13].V[3].x = 0.5f; R[13].V[3].y = -0.75f;
+	//RAend
+	//Neck
+	R[14].V[0].x = -0.25f; R[14].V[0].y = 0;	R[14].V[1].x = -0.25f; R[14].V[1].y = 0.5f;
+	R[14].V[2].x = 0.25f; R[14].V[2].y = 0.5f;	R[14].V[3].x = 0.25f; R[14].V[3].y = 0;
+	//Head
+	R[15].V[0].x = -1; R[15].V[0].y = 0;	R[15].V[1].x = -1; R[15].V[1].y = 2;
+	R[15].V[2].x = 1; R[15].V[2].y = 2;	R[15].V[3].x = 1; R[15].V[3].y = 0;
+
+	
+	int current = 0; 
+	R[current].colors[0] = 1.0f;
 	for(int i = 0; i < 256; i++)
 		keyStates[i] = false;
 
@@ -84,8 +138,8 @@ void drawQuad(Rect R){
 	//, const float* c) {
 
 	glColor3fv(R.colors);
-	glLineWidth(3.0f);
-	glBegin(GL_QUADS);
+	glLineWidth(.1f);
+	glBegin(GL_LINE_LOOP);
 	for (int i = 0; i < 4; i++)
 		glVertex2f(R.V[i].x, R.V[i].y);
 	glEnd();
@@ -111,10 +165,14 @@ void display(void) {
 	drawQuad(R[0]);	//LB
 	glPushMatrix();
 	
+	glTranslatef(-1.5f, 0.0f, 0.0f);
+
 	drawQuad(R[1]); //RT
 		glPushMatrix();
+		glTranslatef(0.0f, -2.0f, 0.0f);
 	drawQuad(R[2]); //RT1
 			glPushMatrix();
+			glTranslatef(0.0f, -2.0f, 0.0f);
 	drawQuad(R[3]); //RT2
 			glPopMatrix();
 		glPopMatrix();
@@ -123,26 +181,33 @@ void display(void) {
 	glPopMatrix();
 	glPushMatrix();
 	
+	glTranslatef(1.5f, 0.0f, 0.0f);
 	drawQuad(R[4]); //LT
 		glPushMatrix();
+		glTranslatef(0.0f, -2.0f, 0.0f);
 	drawQuad(R[5]); //LT1
 			glPushMatrix();
+			glTranslatef(0.0f, -2.0f, 0.0f);
 	drawQuad(R[6]); //LT2
 			glPopMatrix();
 		glPopMatrix();
 
-	//Back to LB, then UB
+	////Back to LB, then UB
 	glPopMatrix();
 
 	glPushMatrix(); //Stores LB, and draws UB with respect to LB
+	glTranslatef(0.0f, 1.0f, 0.0f);
 	drawQuad(R[7]); //UB
 	glPushMatrix(); //Now onwards we wanna draw with respect to UB
 
-	//Push UB, then RA
+	////Push UB, then RA
+	glTranslatef(-3, 1.5f, 0);
 	drawQuad(R[8]); //RA
 		glPushMatrix();
-	drawQuad(R[9]); //RA1
+		glTranslatef(-1.5f,0, 0);
+		drawQuad(R[9]); //RA1
 			glPushMatrix();
+			glTranslatef(-1.5f, 0, 0);
 	drawQuad(R[10]); //RA2
 			glPopMatrix();
 		glPopMatrix(); //After this, we're back at UB
@@ -150,20 +215,25 @@ void display(void) {
 	glPopMatrix();
 	glPushMatrix();
 
+	glTranslatef(3, 1.5f, 0);
 	drawQuad(R[11]); //LA
 		glPushMatrix();
+		glTranslatef(1.5f, 0, 0);
 	drawQuad(R[12]); //LA1
 			glPushMatrix();
+			glTranslatef(1.5f, 0, 0);
 	drawQuad(R[13]); //LA2
 			glPopMatrix();
 		glPopMatrix();
 	
-	//Back to UB, then Neck
+	////Back to UB, then Neck
 	glPopMatrix();
 
 	glPushMatrix(); //To draw the Neck w.r.t UB, we push UB
-	drawQuad(R[14]); //Neck
+	glTranslatef(0.0f, 2, 0.0f);
+	drawQuad(R[14])	; //Neck
 		glPushMatrix(); //To draw Head w.r.t Neck
+		glTranslatef(0.0f, 0.5f, 0.0f);
 	drawQuad(R[15]); //Head
 
 	/// . . . . . 
